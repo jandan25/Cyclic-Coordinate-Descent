bd9525c97cab4b89510ce71c12ec06149b56540c
jandan25 <demidov4800@yandex.ru>
2017-09-17 18:26:52 +0300

    CCD

---
+++ CCD_IK/CalcIK_2D_CCD.xaml.cs
@@ -0,0 +1,546 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CCD_IK
+{
+	/// <summary>
+	/// 2D cyclic coordinate descent based inverse kinematics
+	/// </summary>
+	public partial class CalcIK_2D_CCD : UserControl, INotifyPropertyChanged
+	{
+		#region Internal types
+		
+		// this class represents a bone in it's parent space
+		public class BoneData : INotifyPropertyChanged
+		{
+			private double m_length = 0;
+			private double m_angle = 0;
+
+            // joint limiters
+            private double m_minLimiters = -3.14;
+            private double m_maxLimiters = 3.14;
+
+			#region INotifyPropertyChanged interface
+			// event used by the user interface to bind to our properties
+			public event PropertyChangedEventHandler PropertyChanged;
+
+			// helper function to notify PropertyChanged subscribers
+			protected void NotifyPropertyChanged(string propertyName)
+			{
+				if (PropertyChanged != null)
+				{
+					PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+				}
+			}
+			#endregion
+	
+			public double Length
+			{
+				get { return m_length; }
+				set { m_length = value; NotifyPropertyChanged("Length"); }
+			}
+			public double Radians
+			{ 
+				get { return m_angle; }
+				set { m_angle = value; NotifyPropertyChanged("Radians"); NotifyPropertyChanged("Degrees"); }
+			}
+			public double Degrees
+			{ 
+				get { return m_angle * 180.0 / Math.PI; }
+				set { m_angle = value * Math.PI / 180.0; NotifyPropertyChanged("Radians"); NotifyPropertyChanged("Degrees"); }
+			}
+
+            public double MinLimiter
+            {
+                get { return m_minLimiters; }
+                set { m_minLimiters = value * Math.PI / 180.0; NotifyPropertyChanged("MinLimiter"); }
+            }
+            public double MinLimiterConv
+            {
+                get { return m_minLimiters * 180.0 / Math.PI; ; }
+                set { m_minLimiters = value * Math.PI / 180.0; NotifyPropertyChanged("MinLimiter"); }
+            }
+
+            public double MaxLimiter
+            {
+                get { return m_maxLimiters; }
+                set { m_maxLimiters = value * Math.PI / 180.0; NotifyPropertyChanged("Maxlimiter"); }
+            }
+
+            public double MaxLimiterConv
+            {
+                get { return m_maxLimiters * 180.0 / Math.PI; }
+                set { m_maxLimiters = value * Math.PI / 180.0; NotifyPropertyChanged("Maxlimiter"); }
+            }
+		}
+		
+		#endregion
+
+		#region Private data
+
+		private static Brush[] s_boneLineColors = 
+		{
+			Brushes.Black,
+			Brushes.GreenYellow,
+			Brushes.DarkBlue,
+			Brushes.DarkRed,
+			Brushes.MintCream,
+		};
+
+		// number of CCD iterations to perform on each update
+		private int m_iterationsPerUpdate = 0;
+
+		// bone lengths
+		private ObservableCollection<BoneData> m_bones = new ObservableCollection<BoneData>();
+
+		// lines used to draw the bones
+		private List<Line> m_boneLines = new List<Line>();
+		
+		// target position to reach for
+		private Point m_targetPos = new Point(0,0);
+
+		// max distance from end effector to target for a valid solution
+		private double m_arrivalDist = 1.0;
+
+		// result of current IK calculation
+		private string m_ccdResult = "";
+
+		private DispatcherTimer m_updateTimer;
+
+		#endregion
+
+		#region INotifyPropertyChanged interface
+		// event used by the user interface to bind to our properties
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		// helper function to notify PropertyChanged subscribers
+		protected void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		#endregion
+
+		#region Public properties
+
+		public ObservableCollection<BoneData> Bones
+		{
+			get { return m_bones; }
+		}
+
+		public int IterationsPerUpdate
+		{
+			get { return m_iterationsPerUpdate; }
+			set
+			{
+				m_iterationsPerUpdate = Math.Max(1,value);
+				NotifyPropertyChanged("IterationsPerUpdate"); // update the bound UI
+			}
+		}
+
+		public string CCDResult
+		{
+			get { return m_ccdResult; }
+			set
+			{
+				m_ccdResult = value;
+				NotifyPropertyChanged("CCDResult"); // update the bound UI
+			}
+		}
+			
+		public double TargetPosX
+		{
+			get { return m_targetPos.X; }
+			set
+			{
+				m_targetPos.X = value;
+				NotifyPropertyChanged("TargetPosX"); // update the bound UI
+				UpdateDisplay(); // redraw
+			}
+		}
+
+		public double TargetPosY
+		{
+			get { return m_targetPos.Y; }
+			set
+			{
+				m_targetPos.Y = value;
+				NotifyPropertyChanged("TargetPosY"); // update the bound UI
+				UpdateDisplay(); // redraw
+			}
+		}
+
+		public double ArrivalDist
+		{
+			get { return m_arrivalDist; }
+			set
+			{
+				m_arrivalDist = value;
+				NotifyPropertyChanged("ArrivalDist"); // update the bound UI
+				UpdateDisplay(); // redraw
+			}
+		}
+
+		#endregion
+
+		#region Lifespan functions
+		public CalcIK_2D_CCD()
+		{
+			InitializeComponent();
+			
+			// set the iteration number
+			IterationsPerUpdate = 1;
+
+			// create the timer
+			m_updateTimer = new DispatcherTimer(DispatcherPriority.Normal);
+			m_updateTimer.Tick += new EventHandler(UpdateTimer_Tick);
+			m_updateTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+
+			// add the initial bones
+			AddBone();
+			AddBone();
+            AddBone();
+            AddBone();
+
+
+			TargetPosX = 100;
+			TargetPosY = 100;
+
+			// update the display
+			UpdateDisplay();
+		}
+		#endregion
+		
+		#region Coordinate Conversion
+
+		// compute the logical origin in _viewport coordinated
+		private double ViewportWidth { get { return _viewportColumn.ActualWidth; } }
+		private double ViewportHieght { get { return _mainGrid.ActualHeight; } }
+		private double ViewportCenterX { get { return ViewportWidth / 2; } }
+		private double ViewportCenterY { get { return ViewportHieght / 2; } }
+
+		// convert logical coordinates to _viewport coordinates
+		private double LogicalToViewportX(double logicalX) { return logicalX + ViewportCenterX; }
+		private double LogicalToViewportY(double logicalY) { return -logicalY + ViewportCenterY; }
+	
+		// convert _viewport coordinates to logical coordinates
+		private double ViewportToLogicalX(double viewportX) { return viewportX - ViewportCenterX; }
+		private double ViewportToLogicalY(double viewportY) { return -viewportY + ViewportCenterY; }
+		
+		#endregion
+
+		#region Logic Functions
+
+		/// <summary>
+		/// Add a new bone to the chain at the selected location or at the end if no location is selected.
+		/// </summary>
+		void AddBone()
+		{
+			BoneData newBone = new BoneData();
+			newBone.Length = 50;
+
+			newBone.PropertyChanged += BonePropertyChanged;
+
+			// insert at the end if no bone is selected
+			if( _boneList.SelectedIndex == -1 )
+				Bones.Add( newBone );
+			else
+				Bones.Insert( _boneList.SelectedIndex, newBone );
+		}
+
+		/// <summary>
+		/// Remove a new bone from the chain at the selected location or from the end if no location is selected.
+		/// </summary>
+		private void RemoveBone()
+		{
+			if( Bones.Count == 0 )
+				return;
+
+			// remove the end bone if no bone is selected
+			int removeIdx = _boneList.SelectedIndex;
+			if( removeIdx == -1 )
+				removeIdx = (Bones.Count - 1);
+
+
+			Bones[removeIdx].PropertyChanged -= BonePropertyChanged;
+			Bones.RemoveAt( removeIdx );
+		}
+
+		/// <summary>
+		/// Perform an iteration of IK
+		/// </summary>
+		private void UpdateIK()
+		{
+			int numBones = Bones.Count;
+			
+			if( numBones == 0 )
+				return;
+
+			// calculate the bone angles
+			List< IKSolver.Bone_2D_CCD > ccdBones = new List< IKSolver.Bone_2D_CCD >();
+			for( int boneIdx = 0; boneIdx <= numBones; ++boneIdx )
+			{
+				IKSolver.Bone_2D_CCD newCcdBone = new IKSolver.Bone_2D_CCD();
+				newCcdBone.angle	= (boneIdx < numBones) ? Bones[boneIdx].Radians : 0;
+				newCcdBone.x		= (boneIdx > 0) ? Bones[boneIdx-1].Length : 0;
+				newCcdBone.y		= 0;
+                newCcdBone.minlimiter = (boneIdx < numBones) ? Bones[boneIdx].MinLimiter : 0;
+                newCcdBone.maxlimiter = (boneIdx < numBones) ? Bones[boneIdx].MaxLimiter : 0;
+				ccdBones.Add( newCcdBone );
+			}
+
+			// iterate CCD until limit is reached or we find a valid solution
+			for( int itrCount = 0; itrCount < IterationsPerUpdate; ++itrCount )
+			{
+				IKSolver.CCDResult result = IKSolver.CalcIK_2D_CCD( ref ccdBones, TargetPosX, TargetPosY, ArrivalDist );
+				if( result == IKSolver.CCDResult.Processing )
+				{
+					CCDResult = "Обработка";
+				}
+				else if( result == IKSolver.CCDResult.Success )
+				{
+					CCDResult = "Выполнено";
+					break;
+				}
+				else if( result == IKSolver.CCDResult.Failure )
+				{
+					CCDResult = "Неудача";
+					break;
+				}
+				else
+				{
+					Debug.Assert(false);
+					CCDResult = "[UNKNOWN]";
+					break;
+				}
+			}
+
+			// extract the new bone data from the results
+			for( int boneIdx = 0; boneIdx < numBones; ++boneIdx )
+			{
+				Bones[boneIdx].Radians = ccdBones[boneIdx].angle;
+			}
+		}
+
+		/// <summary>
+		/// Update the scene displayed in the viewport
+		/// </summary>
+		private void UpdateDisplay()
+		{
+			int numBones = Bones.Count;
+			
+			// resize the number of bone lines
+			while( m_boneLines.Count > numBones )
+			{
+				_viewport.Children.Remove( m_boneLines[m_boneLines.Count-1] );
+				m_boneLines.RemoveAt( m_boneLines.Count-1 );
+			}
+
+			while( m_boneLines.Count < numBones )
+			{
+				Line newBoneLine = new Line();
+				newBoneLine.Stroke = s_boneLineColors[m_boneLines.Count % s_boneLineColors.Length];
+				newBoneLine.StrokeThickness	= 3;
+				newBoneLine.SetValue( Panel.ZIndexProperty, 100 );
+
+				m_boneLines.Add( newBoneLine );
+				_viewport.Children.Add( newBoneLine );
+			}
+
+			// compute the orientations of the bone lines in logical space
+			double curAngle = 0;
+			for( int boneIdx = 0; boneIdx < numBones; ++boneIdx )
+			{
+				BoneData curBone = Bones[boneIdx];
+
+				curAngle += curBone.Radians;
+				double cosAngle = Math.Cos( curAngle );
+				double sinAngle = Math.Sin( curAngle );
+
+				if( boneIdx > 0 )
+				{
+					m_boneLines[boneIdx].X1 = m_boneLines[boneIdx-1].X2;
+					m_boneLines[boneIdx].Y1 = m_boneLines[boneIdx-1].Y2;
+				}
+				else
+				{
+					m_boneLines[boneIdx].X1 = 0;
+					m_boneLines[boneIdx].Y1 = 0;
+				}
+
+				m_boneLines[boneIdx].X2 = m_boneLines[boneIdx].X1 + cosAngle*curBone.Length;
+				m_boneLines[boneIdx].Y2 = m_boneLines[boneIdx].Y1 + sinAngle*curBone.Length;
+			}
+
+			// convert the bone positions to viewport space
+			foreach( Line curLine in m_boneLines )
+			{
+				curLine.X1 = LogicalToViewportX(curLine.X1);
+				curLine.Y1 = LogicalToViewportY(curLine.Y1);
+
+				curLine.X2 = LogicalToViewportX(curLine.X2);
+				curLine.Y2 = LogicalToViewportY(curLine.Y2);
+			}
+
+			// draw the arrival distance
+			Canvas.SetLeft( _arrivalEllipse, LogicalToViewportX(TargetPosX - ArrivalDist) );
+			Canvas.SetTop( _arrivalEllipse, LogicalToViewportY(TargetPosY + ArrivalDist) );
+			_arrivalEllipse.Width = 2.0 * ArrivalDist;
+			_arrivalEllipse.Height = 2.0 * ArrivalDist;
+
+			// draw the target
+			Canvas.SetLeft( _targetEllipse, LogicalToViewportX(TargetPosX - _targetEllipse.Width/2) );
+			Canvas.SetTop( _targetEllipse, LogicalToViewportY(TargetPosY + _targetEllipse.Height/2) );
+			
+			// draw the axes
+			_xAxisLine.X1 = 0;
+			_xAxisLine.Y1 = ViewportCenterY;
+			_xAxisLine.X2 = ViewportWidth;
+			_xAxisLine.Y2 = ViewportCenterY;
+
+			_yAxisLine.X1 = ViewportCenterX;
+			_yAxisLine.Y1 = 0;
+			_yAxisLine.X2 = ViewportCenterX;
+			_yAxisLine.Y2 = ViewportHieght;
+		}
+
+		/// <summary>
+		/// Update logic at a set interval
+		/// </summary>
+		private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+			UpdateIK();
+			UpdateDisplay();
+		}
+
+		#endregion
+		
+		#region Event Handlers
+
+		private void BonePropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			switch (e.PropertyName)
+			{
+				case "Radians":
+					UpdateDisplay();
+					break;
+				case "Length":
+					UpdateDisplay();
+					break;
+			}
+		}
+
+		private void viewport_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			// capture the mouse to keep grabing MouseMove events if the user drags the
+			// mouse outside of the _viewport bounds
+			if (!_viewport.IsMouseCaptured)
+            {
+                _viewport.CaptureMouse();
+			}
+
+			// update the target position
+			Point viewportPos = e.GetPosition(_viewport);
+			TargetPosX = ViewportToLogicalX( viewportPos.X );
+			TargetPosY = ViewportToLogicalY( viewportPos.Y );
+		}
+
+		private void viewport_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			// release the captured mouse
+			if (_viewport.IsMouseCaptured)
+            {
+                _viewport.ReleaseMouseCapture();
+            }
+		}
+
+		private void viewport_MouseMove(object sender, MouseEventArgs e)
+		{
+			// update the target position if we are still in a captured state
+			// (i.e. the user has not released the mouse button)
+			if (_viewport.IsMouseCaptured)
+            {
+				Point viewportPos = e.GetPosition(_viewport);
+				TargetPosX = ViewportToLogicalX( viewportPos.X );
+				TargetPosY = ViewportToLogicalY( viewportPos.Y );
+            }
+		}
+
+		private void _thisWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			// update the display shapes based on the new window size
+			UpdateDisplay();
+		}
+		
+		private void _thisWindow_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{			
+			if( m_updateTimer != null )
+			{
+				if( this.IsVisible )
+				{
+					if( _playRadioButton.IsChecked == true )
+						m_updateTimer.Start();
+				}
+				else
+				{
+					m_updateTimer.Stop();
+				}
+			}
+		}
+
+		private void _websiteLink_Click(object sender, RoutedEventArgs e)
+		{
+			System.Diagnostics.Process.Start( "http://www.ryanjuckett.com" );
+		}
+
+		private void _addBoneButton_Click(object sender, RoutedEventArgs e)
+		{
+			AddBone();
+			UpdateDisplay();
+		}
+
+		private void _removeBoneButton_Click(object sender, RoutedEventArgs e)
+		{
+			RemoveBone();
+			UpdateDisplay();
+		}	
+
+		private void _playRadioButton_Checked(object sender, RoutedEventArgs e)
+		{
+			if( m_updateTimer != null )
+				m_updateTimer.Start();
+		}
+
+		private void _pauseRadioButton_Checked(object sender, RoutedEventArgs e)
+		{
+			if( m_updateTimer != null )
+				m_updateTimer.Stop();
+		}
+
+		private void _singleUpdateButton_Click(object sender, RoutedEventArgs e)
+		{
+			_pauseRadioButton.IsChecked = true;
+			UpdateIK();
+			UpdateDisplay();
+		}
+
+		#endregion
+	}
+}
---
+++ CCD_IK/IKSolver.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace CCD_IK
+{
+	class IKSolver
+	{
+		///***************************************************************************************
+		/// SimplifyAngle
+		/// This function will convert an angle to the equivalent rotation in the range [-pi,pi]
+		///***************************************************************************************
+		private static double SimplifyAngle(double angle)
+		{
+			angle = angle % (2.0 * Math.PI);
+			if( angle < -Math.PI )
+				angle += (2.0 * Math.PI);
+			else if( angle > Math.PI )
+				angle -= (2.0 * Math.PI);
+			return angle;
+		}
+
+		///***************************************************************************************
+		/// Bone_2D_CCD_World
+		/// This class is used internally by the CalcIK_2D_CCD function to represent a bone in
+		/// world space.
+		///***************************************************************************************
+		private class Bone_2D_CCD_World
+		{
+			public double x;        // x position in world space
+			public double y;        // y position in world space
+			public double angle;    // angle in world space
+			public double cosAngle; // sine of angle
+			public double sinAngle; // cosine of angle
+		};
+
+		///***************************************************************************************
+		/// CCDResult
+		/// This enum represents the resulting state of a CCD iteration.
+		///***************************************************************************************
+		public enum CCDResult
+		{
+			Success,    // the target was reached
+			Processing, // still trying to reach the target
+			Failure,    // failed to reach the target
+		}
+
+		///***************************************************************************************
+		/// Bone_2D_CCD
+		/// This class is used to supply the CalcIK_2D_CCD function with a bone's representation
+		/// relative to its parent in the kinematic chain.
+		///***************************************************************************************
+		public class Bone_2D_CCD
+		{
+			public double x;     // x позиция в родительском пространстве
+            public double y;     // y позиция в родительском пространстве
+            public double angle; // угол  позиция в родительском пространстве
+            public double minlimiter; // минимальное ограничение
+            public double maxlimiter; // максимальное ограничение
+		};
+
+		///***************************************************************************************
+		/// CalcIK_2D_CCD
+		/// Given a bone chain located at the origin, this function will perform a single cyclic
+		/// coordinate descent (CCD) iteration. This finds a solution of bone angles that places
+		/// the final bone in the given chain at a target position. The supplied bone angles are
+		/// used to prime the CCD iteration. If a valid solution does not exist, the angles will
+		/// move as close to the target as possible. The user should resupply the updated angles 
+		/// until a valid solution is found (or until an iteration limit is met).
+		///  
+		/// returns: CCDResult.Success when a valid solution was found.
+		///          CCDResult.Processing when still searching for a valid solution.
+		///          CCDResult.Failure when it can get no closer to the target.
+		///***************************************************************************************
+		public static CCDResult CalcIK_2D_CCD
+		(
+			ref List<Bone_2D_CCD> bones, // Bone values to update
+			double targetX,              // Target x position for the end effector
+			double targetY,              // Target y position for the end effector
+			double arrivalDist           // Must get within this range of the target
+		)
+		{
+			// Set an epsilon value to prevent division by small numbers.
+			const double epsilon = 0.0001; 
+
+			// Set max arc length a bone can move the end effector an be considered no motion
+			// so that we can detect a failure state.
+			const double trivialArcLength = 0.00001; 
+
+
+			int numBones = bones.Count;
+			Debug.Assert(numBones > 0);
+
+			double arrivalDistSqr = arrivalDist*arrivalDist;
+
+			//===
+			// Generate the world space bone data.
+			List<Bone_2D_CCD_World> worldBones = new List<Bone_2D_CCD_World>();
+
+			// Start with the root bone.
+			Bone_2D_CCD_World rootWorldBone = new Bone_2D_CCD_World();
+			rootWorldBone.x = bones[0].x;
+			rootWorldBone.y = bones[0].y;
+			rootWorldBone.angle = bones[0].angle;
+			rootWorldBone.cosAngle = Math.Cos( rootWorldBone.angle );
+			rootWorldBone.sinAngle = Math.Sin( rootWorldBone.angle );
+			worldBones.Add( rootWorldBone );
+			
+			// Convert child bones to world space.
+			for( int boneIdx = 1; boneIdx < numBones; ++boneIdx )
+			{
+				Bone_2D_CCD_World prevWorldBone	= worldBones[boneIdx-1];
+				Bone_2D_CCD curLocalBone = bones[boneIdx];
+
+				Bone_2D_CCD_World newWorldBone = new Bone_2D_CCD_World();
+				newWorldBone.x = prevWorldBone.x + prevWorldBone.cosAngle*curLocalBone.x
+				                                 - prevWorldBone.sinAngle*curLocalBone.y;
+				newWorldBone.y = prevWorldBone.y + prevWorldBone.sinAngle*curLocalBone.x
+				                                 + prevWorldBone.cosAngle*curLocalBone.y;
+				newWorldBone.angle = prevWorldBone.angle + curLocalBone.angle;
+				newWorldBone.cosAngle = Math.Cos( newWorldBone.angle );
+				newWorldBone.sinAngle = Math.Sin( newWorldBone.angle );
+				worldBones.Add(newWorldBone);
+			}
+			
+			//===
+			// Track the end effector position (the final bone)
+			double endX = worldBones[numBones-1].x;
+			double endY = worldBones[numBones-1].y;
+
+			//===
+			// Perform CCD on the bones by optimizing each bone in a loop 
+			// from the final bone to the root bone
+			bool modifiedBones = false;
+			for( int boneIdx = numBones-2; boneIdx >= 0; --boneIdx )
+			{
+				// Get the vector from the current bone to the end effector position.
+				double curToEndX = endX - worldBones[boneIdx].x;
+				double curToEndY = endY - worldBones[boneIdx].y;
+				double curToEndMag = Math.Sqrt( curToEndX*curToEndX + curToEndY*curToEndY );
+
+				// Get the vector from the current bone to the target position.
+				double curToTargetX = targetX - worldBones[boneIdx].x;
+				double curToTargetY = targetY - worldBones[boneIdx].y;
+				double curToTargetMag = Math.Sqrt(   curToTargetX*curToTargetX
+				                                   + curToTargetY*curToTargetY );
+
+				// Get rotation to place the end effector on the line from the current
+				// joint position to the target postion.
+				double cosRotAng;
+				double sinRotAng;
+				double endTargetMag = (curToEndMag*curToTargetMag);
+				if( endTargetMag <= epsilon )
+				{
+					cosRotAng = 1;
+					sinRotAng = 0;
+				}
+				else
+				{
+					cosRotAng = (curToEndX*curToTargetX + curToEndY*curToTargetY) / endTargetMag;
+					sinRotAng = (curToEndX*curToTargetY - curToEndY*curToTargetX) / endTargetMag;
+				}
+
+				// Clamp the cosine into range when computing the angle (might be out of range
+				// due to floating point error).
+				double rotAng = Math.Acos( Math.Max(-1, Math.Min(1,cosRotAng) ) );
+				if( sinRotAng < 0.0 )
+					rotAng = -rotAng;
+				
+				// Rotate the end effector position.
+				endX = worldBones[boneIdx].x + cosRotAng*curToEndX - sinRotAng*curToEndY;
+				endY = worldBones[boneIdx].y + sinRotAng*curToEndX + cosRotAng*curToEndY;
+
+				// Rotate the current bone in local space (this value is output to the user)
+				bones[boneIdx].angle = SimplifyAngle( bones[boneIdx].angle + rotAng );
+                if (bones[boneIdx].angle != 0)
+                {
+                    if (bones[boneIdx].angle > bones[boneIdx].maxlimiter)
+                        bones[boneIdx].angle = bones[boneIdx].maxlimiter;
+                    if (bones[boneIdx].angle < bones[boneIdx].minlimiter)
+                        bones[boneIdx].angle = bones[boneIdx].minlimiter;
+                }
+                // Check for termination
+				double endToTargetX = (targetX-endX);
+				double endToTargetY = (targetY-endY);
+				if( endToTargetX*endToTargetX + endToTargetY*endToTargetY <= arrivalDistSqr )
+				{
+					// We found a valid solution.
+					return CCDResult.Success;
+				}
+
+				// Track if the arc length that we moved the end effector was
+				// a nontrivial distance.
+				if( !modifiedBones && Math.Abs(rotAng)*curToEndMag > trivialArcLength )
+				{
+					modifiedBones = true;
+				}
+			}
+
+			// We failed to find a valid solution during this iteration.
+			if( modifiedBones )
+				return CCDResult.Processing;
+			else
+				return CCDResult.Failure;
+		}
+	
+	}
+}
